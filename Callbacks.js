7b6d5012c080a1c6c47694adf05e37a1a0629f49
DayanaOviedo2001 <108598745+DayanaOviedo2001@users.noreply.github.com>
2023-04-16 01:33:38 -0500

    Delete Callbacks.js

--- Callbacks.js
+++
@@ -1,5 +0,0 @@
-buscarTransaccion(1, function(resultado) {
-    console.log(`Transacción: ${resultado.transaccion.tipo} - Fecha: ${resultado.transaccion.fecha}`);
-    console.log(`Instructor: ${resultado.instructor.nombre}`);
-    console.log(`Idioma: ${resultado.idioma.nombre}`);
-  });
