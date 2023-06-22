7a86712a99cc1793cf0cd4f3549ea2090dc830b3
DayanaOviedo2001 <108598745+DayanaOviedo2001@users.noreply.github.com>
2023-06-21 21:13:40 -0500

    Add files via upload

---
+++ SEGUNDO PARCIAL-TALLER 7/server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const cors = require('cors');
+
+const { dbConnection } = require('./database/config');
+
+class Server {
+  constructor() {
+    this.app = express.Router();
+    this.router = express.Router();
+
+    this.port = process.env.PORT;
+
+    this.paths = {
+      idiomas: '/api/idiomas',
+      instructores: '/api/instructores',
+      aprendizajes: '/api/aprendizajes',
+    };
+
+    this.connectDB();
+    this.middlewares();
+    this.routes();
+    this.router.use('/v1/inventory', this.app);
+    this._express = express().use(this.router);
+  }
+
+  async connectDB() {
+    await dbConnection();
+  }
+
+  middlewares() {
+    this.app.use(cors());
+    this.app.use(express.json());
+    this.app.use(express.static('public'));
+    this.app.use('/uploads/', express.static('uploads'));
+  }
+
+  routes() {
+    this.app.use(this.paths.idiomas, require('./routes/idiomas'));
+    this.app.use(this.paths.instructores, require('./routes/instructores'));
+    this.app.use(this.paths.aprendizajes, require('./routes/aprendizajes'));
+  }
+}
+
+
+    listen(){
+        this._express.listen(this.port, ()=>{
+            console.log(`Servidor ejecuntando en puerto ${this.port}`);
+            console.log(`Trabajo de Oviedo Insuasti Dayana `);
+        })
+    }
+
+
+}
+
+
+module.exports = Server;
